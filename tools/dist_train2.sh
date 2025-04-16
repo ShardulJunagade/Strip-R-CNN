2ede6e300b52cd6e54ee24d7c11114b8191e2888
Shardul Junagade <23110297@iitgn.ac.in>
2025-04-16 16:58:33 +0530

    my configs

---
+++ tools/dist_train2.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+GPUS=$2
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+# Set the CUDA_VISIBLE_DEVICES to the specified GPUs (GPUS should be a comma-separated string, e.g. "0,2,3")
+export CUDA_VISIBLE_DEVICES=$GPUS
+
+echo "Using GPUs: $CUDA_VISIBLE_DEVICES"
+
+# Ensure GPUS is passed as an integer to nproc_per_node, not as a string like "0,1"
+NUM_GPUS=$(echo $GPUS | tr ',' '\n' | wc -l)
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+nohup python3 -m torch.distributed.run \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$NUM_GPUS \
+    --master_port=$PORT \
+    $(dirname "$0")/train.py \
+    $CONFIG \
+    --seed 0 \
+    --launcher pytorch ${@:3} > my_output.log 2>&1 &
+
+
+# bash tools/dist_train2.sh 'configs_mine/strip_rcnn/strip_rcnn_s_fpn_1x_le90_bihar.py' 2
